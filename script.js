aa80be1cb7539a01516b5936c8e4a36d3e200643
Brian Salomone <brian.j.salomone@gmail.com>
2023-07-18 12:39:33 -0700

    added player object + space free conditional for board

--- script.js
+++ script.js
@@ -13,29 +13,48 @@ const gameBoard = (() => {
     }
 
     const playerMove = (index) => {
-        boardArray.splice(index, 1, PLAYER_SQUARE)
-        console.log("array after player move is " + boardArray)
+        if (spaceFree(boardArray[index])) {
+            boardArray.splice(index, 1, PLAYER_SQUARE)
+            console.log("array is " + boardArray)
+        } else {
+            console.log("space occupied!")
+        }
     }
 
     const opponentMove = (index) => {
         boardArray.splice(index, 1, OPPONENT_SQUARE)
-        console.log("array after opponent move is " + boardArray)
     }
 
+    const spaceFree = (index) => { return index === 0 }
     return {playerMove, opponentMove, boardArray}
 })()
+
+const eventListeners = ((button, index) => {
+    button.addEventListener("click", () => {
+        console.log("clicked button " + index)
+        gameBoard.playerMove(index)
+    })
+})
 
 const boardDom = (() => {
     const content = document.createElement("div")
     content.classList.add("squares")
 
-    //Todo: Nine buttons, each appended to content div.
     for (let i=0; i<gameBoard.boardArray.length; i++) {
         const button = document.createElement("button")
         button.setAttribute("id", "square-button " + (i+1))
         content.appendChild(button)
-        console.log(button)
+
+        eventListeners(button, (i+1))
     }
 
     container.appendChild(content)
 })()
+
+const Player = (name) => {
+    let wins = 0
+    let losses = 0
+    let ties = 0
+
+    return {wins, losses, ties}
+}
